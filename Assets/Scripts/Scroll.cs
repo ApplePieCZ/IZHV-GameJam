925778714b084f399b2c2c3f5a2dd66b79874bf0
ApplePieCZ <lukas1454@seznam.cz>
2022-01-21 19:58:16 +0100

    Added textures and sounds

---
+++ Assets/Scripts/Scroll.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    public float speed = 0.2f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 offset = new Vector2(0, Time.time * speed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
