46e57af87428e2960108856ca43dacde81ba0ce0
ApplePieCZ <lukas1454@seznam.cz>
2022-01-22 15:29:34 +0100

    Implemented coin dropping

---
+++ Assets/Scripts/CoinBehavior.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CoinBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print(other.name);
+        if (other.name is "Despawner2" or "Player") Destroy(gameObject);
+    }
+}
