14c6963a6f84cd7d2b0ce26a7eaeea098ad9855b
ApplePieCZ <lukas1454@seznam.cz>
2022-01-22 15:30:10 +0100

    Implemented Enemy behavior

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public Rigidbody2D rigidBody;
+
+    public Vector2 movement = new Vector2(1f,0f);
+
+    public GameObject coinPrefab;
+
+    public Transform dropPoint;
+    void FixedUpdate()
+    {
+        if (rigidBody.position.x <= -8.4f && movement.x == -1f)
+        {
+            movement.x = 1f;
+        }
+
+        if (rigidBody.position.x >= 4.2f && movement.x == 1f)
+        {
+            movement.x = -1f;
+        }
+        
+        if (rigidBody.position.y <= -4.4f && movement.y == -1f)
+        {
+            movement.y = 0;
+        }
+
+        if (rigidBody.position.y >= 4.4f && movement.y == 1f)
+        {
+            movement.y = 0;
+        }
+        rigidBody.MovePosition(rigidBody.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(gameObject);
+        Instantiate(coinPrefab, dropPoint.position, dropPoint.rotation);
+    }
+}
---
+++ Assets/Scripts/EnemyShooting1.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting1 : MonoBehaviour
+{
+    public Transform[] firePointField;
+    
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
+    
+    public float shootTime;
+
+    public float shootInterval = 0.5f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        shootTime += Time.deltaTime;
+        
+        if (shootTime <= shootInterval)
+        { return; }
+        
+        Shoot(firePointField[0]);
+        Shoot(firePointField[1]);
+        shootTime = 0.0f;
+    }
+    
+    void Shoot(Transform firePoint)
+    {
+        var bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        var rigidBody = bullet.GetComponent<Rigidbody2D>();
+        rigidBody.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+    
+}
